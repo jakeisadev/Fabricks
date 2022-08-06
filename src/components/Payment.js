e7110820d6395a5f0f9e2d2fdbf29f0fb7b9fd22
jakeisadev <starowicz@protonmail.com>
2022-08-05 22:16:26 -0400

    payment page styling overhaul

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -4,33 +4,27 @@ import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 
 const Payment = () => {
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [billing, setBilling] = useState("");
+  const [cardnumber, setCardNumber] = useState("");
+  const [nameoncard, setNameOnCard] = useState("");
+  const [expdate, setExpDate] = useState("");
+  const [cvc, setCvc] = useState("");
   // const [state, setState] = useState('');
   const [zip, setZip] = useState("");
-  const [cardnumber, setCardNumber] = useState("");
-  const [cardexpiration, setCardExpiration] = useState("");
-  const [cvv, setCvv] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     console.log(`Form submitted, 
-                  ${firstname}
-                  ${lastname}
-                  ${email}
-                  ${billing}
-                  ${zip}
                   ${cardnumber}
-                  ${cardexpiration}
-                  ${cvv}`);
+                  ${nameoncard}
+                  ${expdate}
+                  ${cvc}
+                  ${zip}`);
   };
 
   return (
     <div className="form-wrapper">
-      <h2 className="form-title">Enter Information Below</h2>
+      <h2 className="form-title">Finalize Order</h2>
       <div id="pay-form">
         <form
           action="."
@@ -39,68 +33,62 @@ const Payment = () => {
           onSubmit={handleSubmit}
           id="payment-form"
         >
-          <fieldset>
-            <legend aria-hidden="false"></legend>
+            <div className="personal-info-title-div">
+            <h1 aria-label="Pay with card">Pay with card</h1>
+            <img src="./stripe.jpg" alt="Powered by Stripe Payments Logo" className="stripe-img" />
+            </div>
+            <fieldset id="personal-info">
+            <label aria-label="Card Information">Card Information</label>
             <p>
-              <label aria-label="First Name" aria-required="true">
-                First Name:{" "}
-              </label>
               <input
                 type="text"
-                name="first-name"
-                id="first-name"
-                placeholder="First Name"
-                onChange={(event) => setFirstName(event.target.value)}
-                value={firstname}
+                name="card-number"
+                id="card-number"
+                placeholder="0000 0000 0000 0000"
+                onChange={(event) => setCardNumber(event.target.value)}
+                value={cardnumber}
                 required
               ></input>
             </p>
             <p>
-              <label aria-label="Last Name" aria-required="true">
-                Last Name:{" "}
-              </label>
               <input
                 type="text"
-                name="last-name"
-                id="last-name"
-                placeholder="Last name"
-                onChange={(event) => setLastName(event.target.value)}
-                value={lastname}
+                name="nameoncard"
+                id="nameoncard"
+                placeholder="Name On Card"
+                onChange={(event) => setNameOnCard(event.target.value)}
+                value={nameoncard}
+                required
+              ></input>
+            </p>
+            <div className="card-exp-and-cvc-div">
+            <p>
+              <input
+                type="text"
+                name="card-exp-date"
+                id="card-exp-date"
+                placeholder="MM / YY"
+                onChange={(event) => setExpDate(event.target.value)}
+                value={expdate}
                 required
               ></input>
             </p>
             <p>
-              <label aria-label="Email" aria-required="true">
-                Email Address:{" "}
-              </label>
               <input
                 type="text"
-                name="email"
-                id="email"
-                placeholder="Email Address"
-                onChange={(event) => setEmail(event.target.value)}
-                value={email}
+                name="cvc-card"
+                id="cvc-card"
+                placeholder="CVC"
+                onChange={(event) => setCvc(event.target.value)}
+                value={cvc}
                 required
               ></input>
             </p>
+           </div>
+            </fieldset>
+            <fieldset id="normal-info">
+            <label aria-label="Select Your State">Region</label>
             <p>
-              <label aria-label="Billing" aria-required="true">
-                Billing Address:{" "}
-              </label>
-              <input
-                type="text"
-                name="billing"
-                id="billing"
-                placeholder="Billing Address"
-                onChange={(event) => setBilling(event.target.value)}
-                value={billing}
-                required
-              ></input>
-            </p>
-            <p>
-              <label aria-label="Choose State" aria-required="true">
-                State:{" "}
-              </label>
               <select aria-label="Select State">
                 <option value="AL" aria-label="Alabama">
                   Alabama
@@ -258,9 +246,6 @@ const Payment = () => {
               </select>
             </p>
             <p>
-              <label aria-label="Zip Code" aria-required="true">
-                Zip-Code:{" "}
-              </label>
               <Tippy content="Accepts 5-digit Zip-Code.">
                 <input
                   type="number"
@@ -273,72 +258,13 @@ const Payment = () => {
                 ></input>
               </Tippy>
             </p>
-            <p>
-              <label aria-label="Card Number" aria-required="true">
-                Card Number:{" "}
-              </label>
-              <Tippy content="16 numerical digits on front of card.">
-                <input
-                  type="text"
-                  name="card-number"
-                  id="card-number"
-                  placeholder="Card Number"
-                  maxLength={16}
-                  onChange={(event) => setCardNumber(event.target.value)}
-                  value={cardnumber}
-                  required
-                ></input>
-              </Tippy>
-            </p>
-            <p>
-              <label aria-label="Card Expiration" aria-required="true">
-                Card Expiration:{" "}
-              </label>
-              <Tippy content="Format: MM/YY">
-                <input
-                  type="text"
-                  name="card-expiration"
-                  id="card-expiration"
-                  placeholder="Expir."
-                  required
-                  onChange={(event) => setCardExpiration(event.target.value)}
-                  value={cardexpiration}
-                  maxLength={5}
-                ></input>
-              </Tippy>
-            </p>
-            <p>
-              <label aria-label="Card CVV" aria-required="true">
-                *CVV:{" "}
-              </label>
-              <Tippy content="3 Digits on back of card">
-                <input
-                  type="text"
-                  name="card-cvv"
-                  id="card-cvv"
-                  placeholder="CVV"
-                  required
-                  maxLength={3}
-                  onChange={(event) => setCvv(event.target.value)}
-                  value={cvv}
-                ></input>
-              </Tippy>
-            </p>
-          </fieldset>
+            </fieldset>
         </form>
         <div className="btn-payment">
           <button type="submit" id="payment-form">
             Submit
           </button>
         </div>
-      </div>
-      <div className="stripe-div">
-      <p>Powered by </p>
-        <img
-          src="./stripelogo.png"
-          alt="Stripe Payments Logo"
-          className="stripe-logo"
-        />
       </div>
     </div>
   );
